ce41c2107648432791920d9ff78486fe126244fe
giangbang <banggiangle2015@gmail.com>
2022-08-22 17:18:49 +0700

    starter code for distral

---
+++ algs/distral/discrete_distral.py
@@ -0,0 +1,38 @@
+
+from stable_baselines3.common.off_policy_algorithm import OffPolicyAlgorithm
+
+
+
+
+class DiscreteDistral(OffPolicyAlgorithm):
+    
+    def __init__(
+        self,
+        policy: Union[str, Type[DiscreteSACPolicy]],
+        env: Union[GymEnv, str],
+        learning_rate: Union[float, Schedule] = 3e-4,
+        buffer_size: int = 1_000_000,  # 1e6
+        learning_starts: int = 100,
+        batch_size: int = 256,
+        tau: float = 0.005,
+        gamma: float = 0.99,
+        alpha: float = 0.5,
+        beta: float  = 0.5,
+        train_freq: Union[int, Tuple[int, str]] = 1,
+        gradient_steps: int = 1,
+        replay_buffer_class: Optional[ReplayBuffer] = None,
+        replay_buffer_kwargs: Optional[Dict[str, Any]] = None,
+        optimize_memory_usage: bool = False,
+        ent_coef: Union[str, float] = "auto",
+        target_update_interval: int = 1,
+        target_entropy: Union[str, float] = "auto",
+        tensorboard_log: Optional[str] = None,
+        create_eval_env: bool = False,
+        policy_kwargs: Optional[Dict[str, Any]] = None,
+        verbose: int = 0,
+        seed: Optional[int] = None,
+        device: Union[th.device, str] = "auto",
+        _init_setup_model: bool = True,
+    ):
+        
+        
