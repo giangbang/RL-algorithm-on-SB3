1bb968032bd6a2aa49d22b2eed6fe8748434cfc8
giangbang <banggiangle2015@gmail.com>
2022-08-30 14:37:47 +0700

    add some other gridworld envs

--- get_multi_envs.py
+++ get_multi_envs.py
@@ -5,5 +5,12 @@ envs_name = {
     'grid-tworoom': [lambda: gym.make('Gridworld-v0', plan=4),
                      lambda: gym.make('Gridworld-v0', plan=5),
                      lambda: gym.make('Gridworld-v0', plan=6),],
+    'grid-alley': [lambda: gym.make('Gridworld-v0', plan=20),
+                   lambda: gym.make('Gridworld-v0', plan=21),],
+    'grid-maze': [lambda: gym.make('Gridworld-v0', plan=30), 
+                  lambda: gym.make('Gridworld-v0', plan=31),
+                  lambda: gym.make('Gridworld-v0', plan=32),],
+    'grid-seperate-corridor': [lambda: gym.make('Gridworld-v0', plan=50),
+                lambda: gym.make('Gridworld-v0', plan=51),],
 
 }
