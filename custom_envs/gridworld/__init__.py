3f45ef56afc36c4c6eab498b67f50d0e5db84874
giangbang <banggiangle2015@gmail.com>
2022-08-31 09:01:08 +0700

    add training with HER

--- custom_envs/gridworld/__init__.py
+++ custom_envs/gridworld/__init__.py
@@ -1,8 +1,14 @@
-from .gridworld_env import GridworldEnv
+from .gridworld_env import GridworldEnv, MultiGoalGridWorld
 from gym.envs.registration import register
 
 register(
     id="Gridworld-v0",
     entry_point=__name__ + ":GridworldEnv",
+    max_episode_steps=100,
+)
+
+register(
+    id="HER:Gridworld-v0",
+    entry_point=__name__ + ":MultiGoalGridWorld",
     max_episode_steps=100,
 )
---
+++ train_HER.py
@@ -0,0 +1,50 @@
+import gym
+import numpy as np
+
+from algs.distral import DiscreteDistral
+from parse_args import parse_args
+from stable_baselines3.common.logger import configure
+from get_multi_envs import envs_name
+from stable_baselines3.common.vec_env import DummyVecEnv
+from common.callbacks import LogReturnSeparateEnvs
+
+from stable_baselines3 import HerReplayBuffer, DQN
+from stable_baselines3.her.goal_selection_strategy import GoalSelectionStrategy
+from stable_baselines3.common.vec_env import DummyVecEnv
+from stable_baselines3.common.monitor import Monitor
+
+args = parse_args()
+# Available strategies (cf paper): future, final, episode
+goal_selection_strategy = 'future' # equivalent to GoalSelectionStrategy.FUTURE
+# If True the HER transitions will get sampled online
+online_sampling = True
+
+env_name = 'HER:' + args.env_name
+
+log_dir = "/tmp/gym/"
+os.makedirs(log_dir, exist_ok=True)
+
+envs = envs_name[env_name]
+envs = DummyVecEnv(envs)
+envs = Monitor(envs, log_dir)
+
+model = DQN(
+    "MultiInputPolicy",
+    env,
+    replay_buffer_class=HerReplayBuffer,
+    # Parameters for HER
+    replay_buffer_kwargs=dict(
+        n_sampled_goal=4,
+        goal_selection_strategy=goal_selection_strategy,
+        online_sampling=online_sampling,
+    ),
+    verbose=1,
+)
+
+new_logger = configure('./', ["stdout", "csv"])
+model.set_logger(new_logger)
+
+callback_return = LogReturnSeparateEnvs()
+
+model.learn(total_timesteps=args.total_timesteps,
+            log_interval=1000, callback=callback_return)
